dbe57ba768c50c68786b25b8bc469766b73006e7
Fabio Tognarelli <fabiotognaa@gmail.com>
2025-10-27 23:08:00 +0100

    Update slides grafi RO; Aggiunta esercizi Lab 2

---
+++ Corso A/Fabio Tognarelli/Laboratorio II/Esercizi/2025-10-17/2/segnoDiagonale.c
@@ -0,0 +1,59 @@
+// * programma che data una matrice inverte il segno della sua diagonale principale
+#include <stdio.h>
+#include <stdlib.h>
+
+//*funzione per invertire segno della diagonale principale
+int** invertiDiag(int** a, int size){
+    int i = 0, j = 0;
+    for(i = 0; i < size; i++){
+        for(j = 0; j < size; j++){
+            if (i == j){
+                a[i][j] = -a[i][j];
+                break;
+            }
+        }
+    }
+    return a;
+}
+
+int main(){
+
+    int M;
+    int i = 0, j = 0;
+    int val;
+
+    printf("Inserire grandezza matrice: ");
+    scanf("%d", &M);
+
+    int** mat = (int**)malloc(M* sizeof(int*));
+    for(i = 0; i < M; i++){
+        mat[i] = (int*)malloc(M* sizeof(int)); 
+    }
+
+    for(i = 0; i < M; i++){
+        for(j = 0; j < M; j++){
+            printf("Inserire val in posizione %d %d: ", i, j);
+            scanf("%d", &val);
+            mat[i][j] = val;
+        }
+    }
+
+
+    int** ris = invertiDiag(mat, M);
+
+    //*stampa mat invertita
+    for(i = 0; i < M; i++){
+        for(j = 0; j < M; j++){
+            printf("%d ", ris[i][j]);
+        }
+        printf("\n");
+    }
+
+    //*libera memoria
+    for(i = 0; i < M; i++){
+        free(mat[i]);  
+    }
+    free(mat);
+
+    return 0;
+}
---
+++ Corso A/Fabio Tognarelli/Laboratorio II/Esercizi/provaRubata/1/tabelline.c
@@ -0,0 +1,13 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+    int i = 0;
+    //*creazione file per numero di argomenti
+    for(i = 1; i < argc; i++){
+        
+    }
+}
